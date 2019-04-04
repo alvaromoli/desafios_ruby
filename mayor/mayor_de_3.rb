62b4fef48adf7dd46efbc2739c4f39c559ff8c20
Álvaro Molina <abusmolina@gmail.com>
2019-04-04 00:33:32 -0300

    FIX uups

--- mayor/mayor_de_3.rb
+++ mayor/mayor_de_3.rb
@@ -1,5 +1,5 @@
-#Validación de ARGV para recibir solo 3 elementos
-if ARGV.length == 3
+#Validación de ARGV para recibir solo 4 elementos
+if ARGV.length < 5 and ARGV.length > 2
     if ARGV[0].to_i >= ARGV[1].to_i and ARGV[0].to_i >= ARGV[2].to_i
         mayor = ARGV[0].to_i
     elsif ARGV[1].to_i >= ARGV[0].to_i and ARGV[1].to_i >= ARGV[2].to_i 
@@ -9,5 +9,5 @@ if ARGV.length == 3
     end
     puts mayor
 else
-    puts "Los cantidad valores ingresados exceden o son insuficientes, intente ingresar solo 3 valores"
+    puts "Los cantidad valores ingresados exceden o son insuficientes, intente ingresar solo 3 o 4 valores"
 end
