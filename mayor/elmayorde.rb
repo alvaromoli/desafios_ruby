435dd06344d951c828016e5f44ff568df8f1b6f8
Álvaro Molina <abusmolina@gmail.com>
2019-04-04 01:10:11 -0300

    Recorrido del array

--- mayor/elmayorde.rb
+++ mayor/elmayorde.rb
@@ -2,7 +2,13 @@
 if ARGV.length == 1
     puts ARGV[0]
 elsif ARGV.length > 0
-   
+    mayor = 0
+    for i in 0 ... ARGV.length
+        for j in 0 ... ARGV.length
+            
+        end
+    end
+    puts mayor
 else
     puts "Debe ingresar algún parámetro"
 end
