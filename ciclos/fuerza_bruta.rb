f8fc3f7d3f88fb11a4d4b1cd29c85ecda23b5634
Álvaro Molina <abusmolina@gmail.com>
2019-04-10 18:38:28 -0400

    FIX validacion letras

--- ciclos/fuerza_bruta.rb
+++ ciclos/fuerza_bruta.rb
@@ -1,10 +1,14 @@
 def fuerza_bruta (password)
     contador = 0
-    for i in "a"..password do
-        contador += 1
+    if password.match? /\A[a-zA-Z'-]*\z/
+        for i in "a"..password do
+            contador += 1
+        end
+        #Se le resta el intento encontrado en el for
+        puts "#{contador - 1} intentos"
+    else
+        puts "Debe ingresar un string"
     end
-    #Se le resta el intento encontrado en el for
-    puts "#{contador - 1} intentos"
 end
 
 fuerza_bruta(ARGV[0])
