aab3159f2ff42a08a99433bb0d8e8db69b0c91b5
Álvaro Molina <abusmolina@gmail.com>
2019-04-22 21:05:34 -0400

    ADD Desadios dia Lunes HASHES

---
+++ hashes/iter1.rb
@@ -0,0 +1,30 @@
+ventas = {
+    Enero: 15000,
+    Febrero: 22000,
+    Marzo: 12000,
+    Abril: 17000,
+    Mayo: 81000,
+    Junio: 13000,
+    Julio: 21000,
+    Agosto: 41200,
+    Septiembre: 25000,
+    Octubre: 21500,
+    Noviembre: 91000,
+    Diciembre: 21000
+}
+
+
+ventas.each do |key, value|
+    if value > 45000
+        puts value
+    end
+end
+
+
+ventas.each do |key, value|
+    if value > 45000
+        puts key
+    end
+end
+
+
