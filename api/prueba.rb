b1b39522ddfb48325a1a020394aef7454fc997f3
Álvaro Molina <abusmolina@gmail.com>
2019-04-23 20:22:50 -0400

    ADD prueba API NASA

---
+++ api/prueba.rb
@@ -0,0 +1,54 @@
+require 'uri'
+require 'net/http'
+require 'json'
+require 'openssl' 
+
+def request(add, api_key)
+    url = URI(add+"&api_key="+api_key)
+    http = Net::HTTP.new(url.host, url.port)
+
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["cache-control"] = 'no-cache'
+    request["Postman-Token"] = '78f66e42-5b6c-4596-aab7-3f7e4407258d'
+
+    response = http.request(request)
+    JSON.parse response.read_body
+end
+
+def photos_count(data)
+    counts = Hash.new
+    data.each do |x| 
+        counts[x["camera"]["name"]] = 1 + counts[x["camera"]["name"]].to_i
+    end
+    return counts
+end
+
+def buid_web_page(data)
+    html = "<html><head>Curiosity</head><body><ul>"
+    data.each do |photo|
+        html += "<li><img src='" + photo["img_src"] + "'></li>"
+    end
+                        #<li><img src='.../398380645PRCLF0030000CCAM04010L1.PNG'></li>
+                        #<li><img src='.../398381687EDR_F0030000CCAM05010M_.JPG'></li>
+    html += "</ul></body></html>"
+
+    File.write('index.html', html)
+end
+
+#63a0qd5YfwFAtV8oFl5UfqNa8UrlDvcUaeNvNs1g
+
+
+data = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000", "63a0qd5YfwFAtV8oFl5UfqNa8UrlDvcUaeNvNs1g")
+data = data["photos"]
+
+
+
+
+
+
+
+
+
